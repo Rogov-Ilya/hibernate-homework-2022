fb3196ad374d4259f3c430cd013cfe24359404fc
Rogov <Vavner25@gmail.com>
2022-02-15 22:05:10 +0300

    Create homework

--- src/main/java/ru/hh/school/entity/Vacancy.java
+++ src/main/java/ru/hh/school/entity/Vacancy.java
@@ -13,94 +13,110 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "vacancy")
 public class Vacancy {
 
-  private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vacancy_id")
+    private Integer id;
 
-  private Employer employer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
 
-  private Area area;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "area_id")
+    private Area area;
 
-  private String title;
+    @Column(name = "title")
+    private String title;
 
-  private String description;
+    @Column(name = "description")
+    private String description;
 
-  private Integer compensationFrom;
+    @Column(name = "compensation_from")
+    private Integer compensationFrom;
 
-  private Integer  compensationTo;
+    @Column(name = "compensation_to")
+    private Integer  compensationTo;
 
-  private Boolean compensationGross;
+    @Column(name = "compensation_gross")
+    private Boolean compensationGross;
 
-  private LocalDateTime creationTime;
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
 
-  private LocalDateTime archivingTime;
+    @Column(name = "archiving_time")
+    private LocalDateTime archivingTime;
 
-  public Vacancy() {
-  }
+    public Vacancy() {
+    }
 
-  public Vacancy(Employer employer) {
-    this.employer = employer;
-  }
+    public Vacancy(Employer employer) {
+        this.employer = employer;
+    }
 
-  public void setArea(Area area) {
-    this.area = area;
-  }
+    public void setArea(Area area) {
+        this.area = area;
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public Employer getEmployer() {
-    return employer;
-  }
+    public Employer getEmployer() {
+        return employer;
+    }
 
-  public void setEmployer(Employer employer) {
-    this.employer = employer;
-  }
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public void setCompensationFrom(Integer compensationFrom) {
-    this.compensationFrom = compensationFrom;
-  }
+    public void setCompensationFrom(Integer compensationFrom) {
+        this.compensationFrom = compensationFrom;
+    }
 
-  public void setCompensationTo(Integer compensationTo) {
-    this.compensationTo = compensationTo;
-  }
+    public void setCompensationTo(Integer compensationTo) {
+        this.compensationTo = compensationTo;
+    }
 
-  public LocalDateTime getArchivingTime() {
-    return archivingTime;
-  }
+    public LocalDateTime getArchivingTime() {
+        return archivingTime;
+    }
 
-  public void setArchivingTime(LocalDateTime archivingTime) {
-    this.archivingTime = archivingTime;
-  }
+    public void setArchivingTime(LocalDateTime archivingTime) {
+        this.archivingTime = archivingTime;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Vacancy vacancy = (Vacancy) o;
-    return Objects.equals(id, vacancy.id);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return Objects.equals(id, vacancy.id);
+    }
 
-  @Override
-  public int hashCode() {
-    return 17;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
